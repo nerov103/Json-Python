02017792193986d797c4399288cc021ca804fe7a
nerov103 <nibir01000@gmail.com>
2024-02-03 20:06:19 +0600

    Python Convert String to List using split()

---
+++ example.py
@@ -0,0 +1,17 @@
+import json
+
+str_deta = "cd vic"
+
+
+def str_to_list(str_pass):
+    le = str_deta.split()
+    print(le)
+    print(type(le))
+    print(len(le))
+    print(le[1])
+
+str_to_list(str_to_list)
+
+
+
+
